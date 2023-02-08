5acb69655b4794cde1bfb86fbd0bdda0b82db251
goneyacine <goneyacine@gmail.com>
2023-02-08 10:34:42 +0100

    added camera component

--- RenderMe2D/Camera.cpp
+++ RenderMe2D/Camera.cpp
@@ -2,3 +2,38 @@
 
 
 using namespace RenderMe::RenderMe2D;
+
+
+Camera::Camera()
+{
+
+}
+Camera::Camera(RenderMe::Base::Scene* p_scene) :
+	m_scene(p_scene)
+{
+
+}
+Camera::Camera(RenderMe::Base::Scene* p_scene, float p_x, float p_y) :
+	m_scene(p_scene),m_x(p_x),m_y(p_y)
+{
+
+}
+Camera::Camera(RenderMe::Base::Scene* p_scene, float p_x, float p_y, float p_angle) 
+	: m_scene(p_scene),m_x(p_x),m_y(p_y),m_angle(p_angle)
+{
+
+}
+Camera::Camera(RenderMe::Base::Scene* p_scene, float  p_aspectRatioX, float p_aspectRatioY, float p_x, float p_y, float p_angle)
+	: m_scene(p_scene),m_aspectRatioX(p_aspectRatioX),m_aspectRatioY(p_aspectRatioY), m_x(p_x), m_y(p_y), m_angle(p_angle)
+{
+
+}
+Camera::Camera(RenderMe::Base::Scene* p_scene, float p_aspectRatioX, float p_aspectRatioY, float p_orthographicSize, float p_x, float p_y, float p_angle)
+	: m_scene(p_scene), m_aspectRatioX(p_aspectRatioX), m_aspectRatioY(p_aspectRatioY),m_orthographicSize(p_orthographicSize), m_x(p_x), m_y(p_y), m_angle(p_angle)
+{
+
+}
+Camera::~Camera()
+{
+
+}
--- RenderMe2D/Camera.h
+++ RenderMe2D/Camera.h
@@ -1,10 +1,31 @@
 #pragma once
 
+#include "Scene.h"
 
 namespace RenderMe
 {
 	namespace RenderMe2D
 	{
+		class Camera
+		{
+		public :
+			Camera();
+			Camera(RenderMe::Base::Scene* p_scene);
+			Camera(RenderMe::Base::Scene* p_scene, float p_x = 0, float p_y = 0);
+			Camera(RenderMe::Base::Scene* p_scene, float p_x = 0, float p_y = 0,float p_angle = 0);
+			Camera(RenderMe::Base::Scene* p_scene, float  p_aspectRatioX = 16,float p_aspectRatioY = 9,float p_x = 0, float p_y = 0,float p_angle = 0);
+			Camera(RenderMe::Base::Scene* p_scene, float p_aspectRatioX = 16, float p_aspectRatioY = 9,float p_orthographicSize = 10,float p_x = 0, float p_y = 0,float p_angle = 0);
+			~Camera();
+		private :
+			float m_x = 0;
+			float m_y = 0;
+			float m_angle = 0;
 
+			float m_aspectRatioX = 16;
+			float m_aspectRatioY = 9;
+			float m_orthographicSize = 10;
+
+			RenderMe::Base::Scene* m_scene;
+		};
 	}
 }
