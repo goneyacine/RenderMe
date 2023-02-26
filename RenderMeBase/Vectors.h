6984ab8d25cb69b429a6aba9c9a971f9f5fe51bd
goneyacine <goneyacine@gmail.com>
2023-02-26 10:40:24 +0100

    add vectors (2, 3 and 4)

---
+++ RenderMeBase/Vectors.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace RenderMe
+{
+	namespace Base
+	{
+		struct Vector2
+		{
+			float x = 0;
+			float y = 0;
+		};
+		struct Vector3
+		{
+			float x = 0;
+			float y = 0;
+			float z = 0;
+		};
+		struct Vector4
+		{
+			float x = 0;
+			float y = 0;
+			float z = 0;
+			float w = 0;
+		};		    
+	}
+}
