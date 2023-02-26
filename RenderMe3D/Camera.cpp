f3d4152208a62406a5ca663c08263224d793e805
goneyacine <goneyacine@gmail.com>
2023-02-26 10:56:43 +0100

    setting up camera class

--- RenderMe3D/Camera.cpp
+++ RenderMe3D/Camera.cpp
@@ -1,14 +1,49 @@
 #include "Camera.h"
 
+
 using namespace RenderMe::RenderMe3D;
 
 
-Camera::Camera()
+Camera::Camera() :
+	m_scene(nullptr),g_position({0,0,0}),g_rotation({0,0,0})
 {
 
 }
 
+Camera::Camera(RenderMe::Base::Scene* p_scene, RenderMe::Base::Vector3 p_position)
+	: m_scene(p_scene),g_position(p_position),g_rotation({0,0,0})
+{
+}
+
+Camera::Camera(RenderMe::Base::Scene* p_scene, RenderMe::Base::Vector3 p_position, RenderMe::Base::Vector3 p_rotation)
+	: m_scene(p_scene), g_position(p_position), g_rotation(p_rotation)
+{
+}
+
 Camera::~Camera()
+{
+	//deleting index and vertex and index buffers
+	deleteBuffers();
+}
+
+
+void Camera::render()
+{
+	if (m_scene == nullptr)
+		return;
+
+	//initializing vertex and index buffers
+	if (m_vertexBuffer == 0 || m_indexBuffer == 0)
+		initBuffers();
+
+}
+
+void Camera::initBuffers()
+{
+
+}
+
+void Camera::deleteBuffers()
 {
 
 }
--- RenderMe3D/Camera.h
+++ RenderMe3D/Camera.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Scene.h"
+#include "Vectors.h"
 
 namespace RenderMe
 {
@@ -9,7 +11,23 @@ namespace RenderMe
 		{
 		public :
 			Camera();
+			Camera(RenderMe::Base::Scene* p_scene);
+			Camera(RenderMe::Base::Scene* p_scene, RenderMe::Base::Vector3 p_position);
+			Camera(RenderMe::Base::Scene* p_scene, RenderMe::Base::Vector3 P_position, RenderMe::Base::Vector3 p_rotation);
 			~Camera();
+
+
+			void render();
+
+
+			RenderMe::Base::Vector3 g_position;
+			RenderMe::Base::Vector3 g_rotation;
+		private:
+			void initBuffers();
+			void deleteBuffers();
+
+			RenderMe::Base::Scene* m_scene;
+			unsigned int m_vertexBuffer, m_indexBuffer;
 		};
 	}
 }
